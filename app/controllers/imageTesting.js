6d638925630e028645638d5edb920a2475a47224
RobertBowie <robertb1903@gmail.com>
2015-11-27 19:41:23 -0800

    Cleaned up file structure and spacing.

--- app/controllers/imageTesting.js
+++ app/controllers/imageTesting.js
@@ -1,17 +1,54 @@
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d')
+//-----------------------------------------------------------------------
+// Utility Functions:
+//-----------------------------------------------------------------------
 
-var erase = function(){
+var erase = function() {
   context.clearRect(0,0, context.canvas.width, context.canvas.height)
 };
 
-//Get mouse information
+function renderResults(data) {
+  function topTwo (data) {
+    var index;
+    var max;
+    var highestProbs = [];
+
+    function topOne(data) {
+      index = data.indexOf(Math.max.apply(null, data));
+      max = Math.max.apply(null, data);
+      data.splice(index, 1, 0);
+      return [index, (max*100).toPrecision(3) + '%'];
+    };
+
+    highestProbs.push(topOne(data));
+    highestProbs.push(topOne(data));
+    return highestProbs;
+  };
+
+  var highestProbs = topTwo(data);
+
+  var formattedData = '<p>Your number: <span class="digit">'+ highestProbs[0][0] + 
+                      '</span> confidence: <span class="percent">' + 
+                      highestProbs[0][1] + '</span></p> \n' +
+                      '<p>Second possibility: <span class="digit">'+ 
+                      highestProbs[1][0] + '</span> confidence: ' + 
+                      '<span class="percent">' + highestProbs[1][1] +
+                      '</span></p>';
+
+  $('#results').html(formattedData).show;
+};
+
+//-----------------------------------------------------------------------
+// Capture user input:
+//-----------------------------------------------------------------------
+
 var clickX = new Array();
 var clickY = new Array();
 var clickDrag = new Array();
 var paint;
 
-$('#canvas').mousedown(function(e){
+$('#canvas').mousedown(function(e) {
   var mouseX = e.pageX - this.offsetLeft;
   var mouseY = e.pageY - this.offsetTop;
     
@@ -20,39 +57,37 @@ $('#canvas').mousedown(function(e){
   redraw();
 });
 
-$('#canvas').mousemove(function(e){
-  if(paint){
+$('#canvas').mousemove(function(e) {
+  if(paint) {
     addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
     redraw();
   }
 });
 
-$('#canvas').mouseup(function(e){
+$('#canvas').mouseup(function(e) {
   paint = false;
 });
 
-$('#canvas').mouseleave(function(e){
+$('#canvas').mouseleave(function(e) {
   paint = false;
 });
 
-var addClick = function(x,y,dragging){
+var addClick = function(x,y,dragging) {
   clickX.push(x);
   clickY.push(y);
   clickDrag.push(dragging);
-}
+};
 
-var redraw = function(){
+var redraw = function() {
   context.clearRect(0,0, canvas.width, canvas.height);
-  
   context.strokeStyle ="#ff0000";
   context.lineJoin = "round";
   context.lineWidth = 13;
-  
+
   for(var i = 0; i < clickX.length; i++){
     context.beginPath();
     if(clickDrag[i] && i){
       context.moveTo(clickX[i-1], clickY[i-1]);
-    
     } else {
       context.moveTo(clickX[i], clickY[i]);
     }
@@ -64,15 +99,13 @@ var redraw = function(){
 
 //get numerical representation of canvas
 
-var recognize = function(){
-
+var recognize = function() {
   var image = context.getImageData(0,0,canvas.width, canvas.height)
-  
   var shadowCanvas = document.createElement('canvas');
   var shadowContext = shadowCanvas.getContext('2d');
   shadowCanvas.width = 28;
   shadowCanvas.height = 28;
-  
+
   shadowContext.drawImage(canvas, 0,0,280,280,4,4,20,20);
   document.body.appendChild(shadowCanvas);
   var teeny = shadowContext.getImageData(0,0,shadowCanvas.width, shadowCanvas.height)
@@ -84,42 +117,8 @@ var recognize = function(){
   for(var i = 0; i < teeny.data.length; i = i + 4) {
     redValues.push(teeny.data[i]);
   }
-  
+
    console.log(redValues);
-
-  function renderResults(data) {
-    function topTwo (data) {
-      var index;
-      var max;
-      var highestProbs = [];
-
-      function topOne(data) {
-        index = data.indexOf(Math.max.apply(null, data));
-        max = Math.max.apply(null, data);
-        data.splice(index, 1, 0);
-        
-        return [index, (max*100).toPrecision(3) + '%'];
-      };
-
-      highestProbs.push(topOne(data));
-      highestProbs.push(topOne(data));
-
-      return highestProbs;
-    };
-
-    var highestProbs = topTwo(data)
-    //[ [ 8, 1 ], [ 10, 0.99 ] ]
-
-    var formattedData = '<p>Your number: <span class="digit">'+ highestProbs[0][0] + 
-                        '</span> confidence: <span class="percent">' + 
-                        highestProbs[0][1] + '</span></p> \n' +
-                        '<p>Second possibility: <span class="digit">'+ 
-                        highestProbs[1][0] + '</span> confidence: ' + 
-                        '<span class="percent">' + highestProbs[1][1] +
-                        '</span></p>';
-
-    $('#results').html(formattedData).show;
-  };
 
   (function() {
     $.ajax({
@@ -133,6 +132,5 @@ var recognize = function(){
       }
     })
   })();
-
 
 };
